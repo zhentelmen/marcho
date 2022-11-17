9268874abf2f2c9a01b72f271f0a20debfcc7ae1
Roma <german.info@bk.ru>
2022-11-17 16:07:06 +0300

    added product page

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,15 @@
+$('.product-tabs__top-item').on('click', function (e) {
+
+  e.preventDefault;
+  $('.product-tabs__top-item').removeClass('product-tabs__top-item--active');
+  $(this).addClass('product-tabs__top-item--active');
+
+  $('.product-tabs__content-item').removeClass('product-tabs__content-item--active');
+  $($(this).attr('href')).addClass('product-tabs__content-item--active');
+
+});
+
+
 $(function () {
   $('.top-slider__inner').slick({
     dots: true,
